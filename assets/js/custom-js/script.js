c6af3a30ca3bf36e7f99fb3dfe65f16e69c9ed3a
Arpita Pal <arpita.pal@consectus.com>
2025-08-01 15:54:32 +0530

    main-website

---
+++ assets/js/custom-js/script.js
@@ -0,0 +1,45 @@
+$(".package-slider").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navContainer: ["#package-carousel-nav"],
+  navText: [$("#package-nav-left"), $("#package-nav-right")],
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplaySpeed: 1500,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    769: {
+      items: 2,
+    },
+    992: {
+      items: 3,
+    },
+    1441: {
+      items: 4,
+    },
+  },
+});
+
+
+ document.addEventListener("DOMContentLoaded", function () {
+
+    let currentPath = window.location.pathname.split("/").pop();
+
+    if (currentPath === "") currentPath = "index.html";
+
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach((link) => {
+      const linkPath = link.getAttribute("href");
+      if (linkPath === currentPath) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+  });
